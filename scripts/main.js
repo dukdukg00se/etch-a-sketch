9f042933ac09124e962e41aefcc9755f4dee48e5
Will H <will.wkhu@gmail.com>
2021-11-03 00:25:54 -0700

    Boilerplate HTML for TOP etch-a-sketch project as well as a JS file with a function that makes desired size grid and some initial css styling for the grid and boxes.

---
+++ scripts/main.js
@@ -0,0 +1,28 @@
+const container = document.querySelector('#container');
+
+
+
+
+function createGrid(num) {
+  for (let rows = 0; rows < num; rows++) {
+    for (let columns = 0; columns < num; columns++) {
+      const box = document.createElement('div');
+      box.classList.add('box');
+      container.append(box);
+
+      box.style.width = `${500 / num}px`;
+      box.style.height = `${500 / num}px`;
+    
+      console.log(box.style.width);
+    }
+  }
+
+}
+
+
+
+
+
+
+
+createGrid(5);
