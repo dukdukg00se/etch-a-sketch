8c1b4da3ae989b75d679e11aa3b94a6f2fac0587
Will H <will.wkhu@gmail.com>
2021-11-07 00:13:21 -0700

    Added page breakpoint and adjusted styling. Tested out some functions to link pen input color to grid boxes.

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,4 @@
-const container = document.querySelector('#grid-container');
-
-
+const gridContainer = document.querySelector('#grid-container');
 
 
 function createGrid(num) {
@@ -8,21 +6,61 @@ function createGrid(num) {
     for (let columns = 0; columns < num; columns++) {
       const box = document.createElement('div');
       box.classList.add('box');
-      container.append(box);
+      gridContainer.append(box);
 
       box.style.width = `${500 / num}px`;
       box.style.height = `${500 / num}px`;
-    
-      console.log(box.style.width);
     }
   }
+}
+createGrid(4);
 
+
+/*
+const penColor = document.getElementById('pen-color-input');
+penColor.addEventListener('input', (e) => {
+  //const boxColor = document.querySelector('.box');
+  //boxColor.style.cssText = ''
+  
+  console.log(e.target);
+})
+
+const penColor = document.getElementById('pen-color-input');
+console.log(penColor.value);
+*/
+
+
+const penColor = document.getElementById('pen-color-input');
+penColor.addEventListener("input", updateFirst, false);
+//penColor.addEventListener("change", watchColorPicker, false);
+
+function watchColorPicker(event) {
+  document.querySelectorAll(".color-pick-buttons").forEach(function(cpb) {
+    cpb.style.color = event.target.value;
+  });
 }
 
 
+function updateFirst(event) {
+  const boxColor = document.querySelectorAll('.box');
+  console.log(event.target.value);
+  
+  if (penColor) {
+    boxColor.forEach(function(box) {
+      box.style.backgroundColor = event.target.value;
+    })
+  }
+  /*
+  if (penColor) {
+    boxColor.style.backgroundColor = event.target.value;
+  }
+  */
+}
+/*
+function updateAll(event) {
+  document.querySelectorAll(".box").forEach(function(box) {
+    box.style.backgroundColor = event.target.value;
+  });
+}
+*/
 
-
-
-
-
-createGrid(8);
